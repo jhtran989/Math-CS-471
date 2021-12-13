338edda78c4c4c5374a97558264da1a847231292
John Tran <jtran989@unm.edu>
2021-12-13 14:11:03 -0700

    strong scaling for grid size 128 and tol 1e-4

--- homeworks/hw6/hw6_run_script.sh
+++ homeworks/hw6/hw6_run_script.sh
@@ -58,10 +58,15 @@ parallel_strong_python="hw6_parallel_strong.py"
 
 # Record timings for strong scaling -- REMEMBER TO UPDATE BELOW
 # now run multiiple strong scaling studies at once with different tolerances to compare timings...
-num_strong=4
-parallel_strong_dir_array=("parallel_strong_256_1e-4/" "parallel_strong_256_1e-8/" "parallel_strong_512_1e-4/" "parallel_strong_512_1e-8/")
+# num_strong=4
+# parallel_strong_dir_array=("parallel_strong_256_1e-4/" "parallel_strong_256_1e-8/" "parallel_strong_512_1e-4/" "parallel_strong_512_1e-8/")
+# parallel_strong_script="hw6_parallel_strong.pbs"
+# parallel_strong_python_array=("hw6_parallel_strong_256_1e-4.py" "hw6_parallel_strong_256_1e-8.py" "hw6_parallel_strong_512_1e-4.py" "hw6_parallel_strong_512_1e-8.py")
+
+num_strong=1
+parallel_strong_dir_array=("parallel_strong_128_1e-4/")
 parallel_strong_script="hw6_parallel_strong.pbs"
-parallel_strong_python_array=("hw6_parallel_strong_256_1e-4.py" "hw6_parallel_strong_256_1e-8.py" "hw6_parallel_strong_512_1e-4.py" "hw6_parallel_strong_512_1e-8.py")
+parallel_strong_python_array=("hw6_parallel_strong_128_1e-4.py")
 
 # changed from run_script_inner_prod
 function run_serial_error_script() {
